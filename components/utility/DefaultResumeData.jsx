ec0f3ce0e95acece71877eaa54060e1e644cf360
AbdulRahman El-zahaby <el3zahaby@gmail.com>
2024-08-27 18:45:11 +0300

    edit defult data

--- components/utility/DefaultResumeData.jsx
+++ components/utility/DefaultResumeData.jsx
@@ -1,88 +1,98 @@
 const DefaultResumeData = {
-  name: "MARCUS HALL",
-  position: "Developer",
-  contactInformation: "+1-555-0100",
-  email: "beddylea@gmail.com",
-  address: "San Francisco, CA",
-  profilePicture: "",
-  socialMedia: [
+  "name": "AbdulRahman Elzahaby",
+  "position": "Full-Stack Team Lead",
+  "contactInformation": "+201033413023, +966537118696",
+  "email": "el3zahaby@gmail.com",
+  "address": "",
+  "profilePicture": "",
+  "socialMedia": [
     {
-      socialMedia: "Github",
-      link: "github.com//bedivere-lea",
+      "socialMedia": "LinkedIn",
+      "link": "http://www.linkedin.com/in/abdulrahman-el-zahaby"
     },
     {
-      socialMedia: "LinkedIn",
-      link: "linkedin.com/in/bedivere-lea",
-    },
-    {
-      socialMedia: "Website",
-      link: "bedivere-lea.github.io",
-    },
-  ],
-  summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
-  education: [
-    {
-      "school": "Bachelor of Computer Science",
-      "degree": "New York University",
-      "startYear": "2020-08-20",
-      "endYear": "2024-07-01"
-    },
-  ],
-  workExperience: [
-    {
-      "company": "Torph TTC",
-      "position": "Developer",
-      "description": "Torph TTC is a global software company that offers user interface UI development tools and components for a range of developer applications across all platforms.",
-      "keyAchievements": "Created and maintained 10 web applications for numerous national and foreign clients.\nEnsured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.\nCreated and analyzed 500 unit test cases.\nDeveloped python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.\nEstablished and lead a team of 10 people; covering every key role in the early stages.",
-      "startYear": "2023-02-16",
-      "endYear": "2023-02-21"
-    },
-    {
-      "company": "Reilty Group",
-      "position": "Front End Web Developer",
-      "description": "Reilty Group is an industry-leading provider of online gambling software and solutions.",
-      "keyAchievements": "Increased by 35% the reach of users to the platform, over the installation of the web platform in mobile devices.\nDelivered 30 web solutions.\nReplaced 1 full-time web developer under part-time employment achieving a productivity gain of over 60% as measured by working hours.\nWorked with more than 4 different teams at different points of time and provided end-to-end solutions for clients.",
-      "startYear": "2023-02-22",
-      "endYear": "2023-03-09"
-    },
-    {
-      "company": "HellX",
-      "position": "Python Developer",
-      "description": "Heller is a Software Editor specialized in logistics management.",
-      "keyAchievements": "Developed several automation systems - saved over $50,000 annually.\nJoined the team as #10 employee as a developer working directly with the biggest customers.\nInvolved in coding as per the requirement, delivered well ahead of time which saved project cost by 20%.\nReduced database size by nearly 50% and improved its performance by 20%.\nDelivery time of good reduced by 50%.",
-      "startYear": "2023-02-22",
-      "endYear": "2023-03-09"
+      "socialMedia": "Stack Overflow",
+      "link": "https://stackoverflow.com/users/7511165/a-el-zahaby"
     }
   ],
-  projects: [],
-  skills: [
+  "summary": "Experienced software engineer and team member with strong coding skills and passion for developing innovative programs that enhance organizational success. Skilled in managing teams, creating user-friendly software, and communicating effectively. Strategic thinker and innovative creator with the ability to develop customized software to meet the needs of a company.",
+  "education": [
     {
-      title: "Technical Skills",
-      skills: [
-        "JavaScript", "Python", "Web Services", "C++", "HTML5", "CSS", "SQL", "User Interface", "Creativity"
+      "school": "Faculty of Business, Ain Shams University",
+      "degree": "Business Administration Department",
+      "startYear": "2020-10",
+      "endYear": "Present"
+    }
+  ],
+  "workExperience": [
+    {
+      "company": "Lockers",
+      "position": "Backend Team Lead",
+      "description": "Played a crucial role in designing, developing, and maintaining innovative software solutions using Laravel, Elasticsearch, Twilio, and Git.",
+      "keyAchievements": "Led and mentored a team of developers to deliver high-quality software solutions.\nCollaborated with cross-functional teams to analyze user requirements and transform them into scalable, efficient software solutions.\nWrote clean, maintainable code while implementing SOLID principles and best practices.",
+      "startYear": "2023-11",
+      "endYear": "Present"
+    },
+    {
+      "company": "Cloudypedia",
+      "position": "Senior Software Engineer",
+      "description": "Collaborated with cross-functional teams to design and develop user-friendly portals using Drupal PHP, enhancing accessibility and user experience.",
+      "keyAchievements": "Actively contributed to the integration of Apigee's API management solutions, ensuring seamless API access, security, and analytics.\nAssisted in creating and maintaining documentation and guides for developers, facilitating quick adoption and implementation of APIs.",
+      "startYear": "2023-08",
+      "endYear": "Present"
+    },
+    {
+      "company": "Amit Learning",
+      "position": "Lead Software Engineer",
+      "description": "Led a team of full-stack developers in the design, development, and maintenance of multiple web applications using front-end and back-end technologies such as PHP Laravel, JavaScript, VueJs, and MongoDB.",
+      "keyAchievements": "Implemented agile development methodologies and fostered a culture of continuous improvement, knowledge sharing, and innovation within the team.",
+      "startYear": "2023-02",
+      "endYear": "2023-08"
+    },
+    {
+      "company": "Unidevs",
+      "position": "Lead Software Engineer",
+      "description": "Oversaw project strategies and processes, coordinated tasks between team members, maintained technical documentation, and designed equipment upgrades.",
+      "keyAchievements": "Delivered high-quality, scalable software solutions and improved team productivity through efficient project management.",
+      "startYear": "2021",
+      "endYear": "2023"
+    },
+    {
+      "company": "Freelancer",
+      "position": "Full-Stack Web Developer",
+      "description": "Worked on various projects, leading teams to deliver high-quality software solutions under tight deadlines.",
+      "keyAchievements": "Delivered multiple projects including Hermosa App, Balaa Municipality, Pure Center, and Combi Egypt, using technologies like Laravel, MySQL, JavaScript, and Python.",
+      "startYear": "2017",
+      "endYear": "Present"
+    }
+  ],
+  "projects": [],
+  "skills": [
+    {
+      "title": "Technical Skills",
+      "skills": [
+        "PHP", "NodeJS", "Python", "TypeScript", "Laravel", "Symfony", "Drupal", "Django", "OpenCV", "ExpressJS", "VueJS 3", "NuxtJS", "PWA", "Agile development methodologies", "Team leadership", "Git", "GitHub", "Bash", "Linux Administration", "Docker", "RESTful API", "GraphQL", "Data Structure", "SOLID", "Algorithms", "Bootstrap", "Tailwind CSS", "Materialize", "Trello", "Clickup", "Jira", "Slack", "MySQL", "Postgres", "Firebase", "Elasticsearch"
       ]
     },
     {
-      title: "Soft Skills",
-      skills: [
+      "title": "Soft Skills",
+      "skills": [
         "Collaboration", "Problem-solving", "Communication", "Time management", "Result-oriented"
       ]
     },
     {
-      title: "Additional Skills",
-      skills: [
+      "title": "Additional Skills",
+      "skills": [
         "Public Speaking", "Writing", "Research"
-      ] 
+      ]
     }
   ],
-  languages: [
-    "English",
-    "Hindi",
+  "languages": [
+    "Arabic",
+    "English"
   ],
-  certifications: [
-    "Certified Web Professional-Web Developer",
-    "Java Development Certified Professional",
-  ],
-};
+  "certifications": []
+}
+;
 
 export default DefaultResumeData;
