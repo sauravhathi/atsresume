e380fadcfaed280c1464240d93235e2f637db70d
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-03 21:51:43 +0200

    Move source code files to the 'src' folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
