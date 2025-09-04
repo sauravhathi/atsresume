d780ddf5cff7ed48f675b55c1bbd0d454cb60d24
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-04 18:24:19 +0200

    Extract the review left side context into a separate file LeftSide.jsx.

--- src/components/preview/ui/Preview.jsx
+++ src/components/preview/ui/Preview.jsx
@@ -11,6 +11,7 @@ import Language from "../components/Language";
 import Certification from "../components/Certification";
 import ModalHighlightMenu from "../components/ModalHighlightMenu";
 import Header from "../components/Header";
+import LeftSide from "../components/LeftSide";
 
 const DragDropContext = dynamic(
   () =>
@@ -112,70 +113,8 @@ const Preview = () => {
           <hr className="border-dashed my-2"/>
           {/* two column start */}
           <div className="grid grid-cols-3 gap-6">
-            <div className="col-span-1 space-y-2">
-              {resumeData.summary.length > 0 && (
-                <div className="mb-1">
-                  <h2 className="section-title mb-1 border-b-2 border-gray-300">
-                    Summary
-                  </h2>
-                  <p className="content break-words">{resumeData.summary}</p>
-                </div>
-              )}
-              <div>
-                {resumeData.education.length > 0 && (
-                  <div className="mb-1">
-                    <h2 className="section-title mb-1 border-b-2 border-gray-300">
-                      Education
-                    </h2>
-                    {resumeData.education.map((item, index) => (
-                      <div key={index} className="mb-1">
-                        <p className="content i-bold">{item.degree}</p>
-                        <p className="content">{item.school}</p>
-                        <DateRange
-                          startYear={item.startYear}
-                          endYear={item.endYear}
-                          id={`education-start-end-date`}
-                        />
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-              <Droppable droppableId="skills" type="SKILLS">
-                {(provided) => (
-                  <div {...provided.droppableProps} ref={provided.innerRef}>
-                    {resumeData.skills.map((skill, index) => (
-                      <Draggable
-                        key={`SKILLS-${index}`}
-                        draggableId={`SKILLS-${index}`}
-                        index={index}
-                      >
-                        {(provided, snapshot) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className={`mb-1 ${
-                              snapshot.isDragging &&
-                              "outline-dashed outline-2 outline-gray-400 bg-white"
-                            }`}
-                          >
-                            <Skills title={skill.title} skills={skill.skills}/>
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
-              <Language title="Languages" languages={resumeData.languages}/>
-              <Certification
-                title="Certifications"
-                certifications={resumeData.certifications}
-              />
-            </div>
-
+            <LeftSide resumeData={resumeData} />
+            {/* Right side start */}
             <div className="col-span-2 space-y-2">
               {resumeData.workExperience.length > 0 && (
                 <Droppable droppableId="work-experience" type="WORK_EXPERIENCE">
@@ -372,6 +311,7 @@ const Preview = () => {
                 </Droppable>
               )}
             </div>
+            {/* Rigth side end */}
 
           </div>
         </DragDropContext>
