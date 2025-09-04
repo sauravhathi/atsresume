3c591c105df472eb310a46db86122b5334263482
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-04 18:16:01 +0200

    Extract the review header into a separate file Header.jsx.

--- src/components/preview/ui/Preview.jsx
+++ src/components/preview/ui/Preview.jsx
@@ -1,11 +1,8 @@
 /* eslint-disable react/jsx-no-undef */
 import {FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaYoutube,} from "react-icons/fa";
-import {MdEmail, MdLocationOn, MdPhone} from "react-icons/md";
 import {CgWebsite} from "react-icons/cg";
 import Skills from "../components/Skills";
 import DateRange from "../../utility/DateRange";
-import ContactInfo from "../components/ContactInfo";
-import Image from "next/image";
 import Link from "next/link";
 import React, {useContext, useState} from "react";
 import {ResumeContext} from "../../builder";
@@ -13,6 +10,7 @@ import dynamic from "next/dynamic";
 import Language from "../components/Language";
 import Certification from "../components/Certification";
 import ModalHighlightMenu from "../components/ModalHighlightMenu";
+import Header from "../components/Header";
 
 const DragDropContext = dynamic(
   () =>
@@ -110,57 +108,7 @@ const Preview = () => {
       <A4PageWrapper>
         <ModalHighlightMenu/>
         <DragDropContext onDragEnd={onDragEnd}>
-          <div className="f-col items-center mb-1">
-            {resumeData.profilePicture.length > 0 && (
-              <div className="w-24 h-24 rounded-full overflow-hidden border-2 border-fuchsia-700">
-                <Image
-                  src={resumeData.profilePicture}
-                  alt="profile"
-                  width={100}
-                  height={100}
-                  className="object-cover h-full w-full"
-                />
-              </div>
-            )}
-            <h1 className="name">{resumeData.name}</h1>
-            <p className="profession">{resumeData.position}</p>
-            <ContactInfo
-              mainclass="flex flex-row gap-1 mb-1 contact"
-              linkclass="inline-flex items-center gap-1"
-              teldata={resumeData.contactInformation}
-              emaildata={resumeData.email}
-              addressdata={resumeData.address}
-              telicon={<MdPhone/>}
-              emailicon={<MdEmail/>}
-              addressicon={<MdLocationOn/>}
-            />
-            <div className="grid grid-cols-3 gap-1">
-              {resumeData.socialMedia.map((socialMedia, index) => {
-                return (
-                  <a
-                    href={`http://${socialMedia.link}`}
-                    aria-label={socialMedia.socialMedia}
-                    key={index}
-                    title={socialMedia.socialMedia}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="inline-flex items-center gap-1 social-media align-center justify-center "
-                    // Prevent text overflowing, If the socialMedia.link string is longer than 32 characters, apply the wordWrap and display styles to this <a> tag.
-                    // wordWrap: "break-word" breaks the text onto the next line if it's too long,
-                    // display: "inline-block" is necessary for wordWrap to work on an inline element like <a>.
-
-                  >
-                    {icons.map((icon, index) => {
-                      if (icon.name === socialMedia.socialMedia.toLowerCase()) {
-                        return <span key={index}>{icon.icon}</span>;
-                      }
-                    })}
-                    {socialMedia.link}
-                  </a>
-                );
-              })}
-            </div>
-          </div>
+          <Header resumeData={resumeData} icons={icons}/>
           <hr className="border-dashed my-2"/>
           {/* two column start */}
           <div className="grid grid-cols-3 gap-6">
