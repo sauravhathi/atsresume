f913f3047e3a4e9e41f1efa25efab1f61ea89212
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-09 17:26:09 +0200

    Add a condition to the ContactInfo.jsx. Display fields only with data.

--- src/components/preview/components/ContactInfo.jsx
+++ src/components/preview/components/ContactInfo.jsx
@@ -1,25 +1,31 @@
-import React, {  } from "react";
+import React from "react";
 
-const ContactInfo = ({ mainclass, linkclass, teldata, emaildata, addressdata, telicon, emailicon, addressicon }) => {
-    return (
-      <div className={mainclass}>
+const ContactInfo = ({mainclass, linkclass, teldata, emaildata, addressdata, telicon, emailicon, addressicon}) => {
+  return (
+    <div className={mainclass}>
+      {teldata &&
         <a className={linkclass}
-          aria-label="Phone Number"
-          href={`tel:${teldata}`}>
-          {telicon}  {teldata}
+           aria-label="Phone Number"
+           href={`tel:${teldata}`}>
+          {telicon} {teldata}
         </a>
+      }
+      {emaildata &&
         <a className={linkclass}
-          aria-label="Email Address"
-          href={`mailto:${emaildata}`}>
+           aria-label="Email Address"
+           href={`mailto:${emaildata}`}>
           {emailicon} {emaildata}
         </a>
+      }
+      {addressdata &&
         <address
           aria-label="Address"
-          className={linkclass + " not-italic"} >
+          className={linkclass + " not-italic"}>
           {addressicon} {addressdata}
         </address>
-      </div>
-    );
-  }
+      }
+    </div>
+  );
+}
 
 export default ContactInfo;
