46418a83e717862ee36819f684a437251dd23b91
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-12 22:18:52 +0200

    Add metadata for favicon, title, and description; update favicon structure.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css';
 
+export const metadata = {
+  icons: {
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
+  },
+  title: 'IU Resume Editor',
+  description: 'Create your resume with ease',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
